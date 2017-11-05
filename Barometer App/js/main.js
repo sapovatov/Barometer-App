ad298a8075b6e889cf836309ba5f0451c532be8a
Alexey Sapovatov <sapovatov@gmail.com>
2017-11-05 13:23:15 +0200

    Include GSAP into the project

--- Barometer App/js/main.js
+++ Barometer App/js/main.js
@@ -1,4 +1,9 @@
 ﻿// Your code here!
-jQuery(document).load{
-    
-};
+jQuery(document).ready(function () {
+    var main_arrow = jQuery("#Main");
+    TweenLite.to(main_arrow, 1, {
+        attr: {
+            transform:"rotate(60, 250 250)"
+        }
+    });
+});
